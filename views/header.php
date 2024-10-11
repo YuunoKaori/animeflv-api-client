24825d5f10b67bb0e2bdffc25e97abb3ab249536
YuunoKaori <93011454+YuunoKaori@users.noreply.github.com>
2024-10-11 18:51:20 -0500

    Create header.php

---
+++ views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Anime</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="assets/js/scripts.js" defer></script>
+</head>
+<body>
+<header>
+    <h1>Bienvenido a la Página de Anime</h1>
+</header>
